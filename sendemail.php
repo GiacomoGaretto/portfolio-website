2385e306f85a541763a3b734237fbca0d3b4c9f8
GiacomoGaretto <124433834+GiacomoGaretto@users.noreply.github.com>
2023-02-04 14:02:17 +0100

    saving to github

--- sendemail.php
+++ sendemail.php
@@ -1,23 +1,41 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['Message'];
 
-  $to = "giacomo.garetto@gmail.com";
-  $subject = "New Contact Form Submission";
-  $headers = "From: " . $email . "\r\n";
-  $headers .= "Reply-To: " . $email . "\r\n";
-  $headers .= "MIME-Version: 1.0\r\n";
-  $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+  // Validate the form data
+  if (empty($name) || empty($email) || empty($message)) {
+    echo "Please fill in all fields.";
+    exit;
+  }
 
-  $email_body = "You have received a new message from the contact form on your website.\n\n" .
-                "Name: " . $name . "\n" .
-                "Email: " . $email . "\n" .
-                "Message: " . $message . "\n";
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Please enter a valid email address.";
+    exit;
+  }
 
-  mail($to, $subject, $email_body, $headers);
+  // Recipient email address
+  $to = 'recipient@example.com';
+
+  // Subject of the email
+  $subject = 'New message from ' . $name;
+
+  // Message to be sent in the email
+  $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Message: ' . $message;
+
+  // Additional headers
+  $headers = 'From: ' . $email . "\r\n" .
+    'Reply-To: ' . $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  // Send the email
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Email sent successfully!";
+  } else {
+    echo "Failed to send email. Please try again later.";
+  }
 }
 
 ?>
